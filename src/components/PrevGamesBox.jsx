1aa4eb7ff96f6ff33bfa820b1d93bb739ea8e748
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-01-19 12:59:59 +0100

    games history fixed

--- src/components/PrevGamesBox.jsx
+++ src/components/PrevGamesBox.jsx
@@ -9,8 +9,8 @@ const PrevGamesBox = (props) => {
 
   useEffect(() => {
     if (props.prevFields !== lastFields) {
-      setMyArr(props.prevFields.reverse());
       lastFields = props.prevFields;
+      setMyArr([...lastFields].reverse());
     }
     setIsLoaded(true);
   }, [props.prevFields]);
@@ -18,43 +18,72 @@ const PrevGamesBox = (props) => {
   return (
     <>
       {isLoaded > 0 && (
-        <div>
+        <div className='w-75 d-flex flex-column align-items-center opacity-50'>
           {myArr.map((game) => {
             return (
-              <div key={game.id} className='my-3'>
-                <Row xs={3}>
-                  <Col className='border-end border-bottom border-1 border-black'>
-                    {game.row1.cel1}
-                  </Col>
-                  <Col className='border-start border-end border-bottom border-1 border-black'>
-                    {game.row1.cel2}
-                  </Col>
-                  <Col className='border-start border-bottom border-1 border-black'>
-                    {game.row1.cel3}
-                  </Col>
-                </Row>
-                <Row xs={3}>
-                  <Col className='border-top border-bottom border-end border-1 border-black'>
-                    {game.row2.cel1}
-                  </Col>
-                  <Col className='border border-1 border-black'>
-                    {game.row2.cel2}
-                  </Col>
-                  <Col className='border-start border-top border-bottom border-1 border-black'>
-                    {game.row2.cel3}
-                  </Col>
-                </Row>
-                <Row xs={3}>
-                  <Col className='border-top border-end border-1 border-black'>
-                    {game.row3.cel1}
-                  </Col>
-                  <Col className='border-start border-top border-end border-1 border-black'>
-                    {game.row3.cel2}
-                  </Col>
-                  <Col className='border-start border-top border-1 border-black'>
-                    {game.row3.cel3}
-                  </Col>
-                </Row>
+              <div key={'ciao'} className='w-50 display-4 fw-normal my-4'>
+                <div>
+                  <Row xs={3}>
+                    <Col
+                      style={{ aspectRatio: '1/1' }}
+                      className='d-flex justify-content-center align-items-center border-end border-bottom border-1 border-black'
+                    >
+                      {game.row1.cel1}
+                    </Col>
+                    <Col
+                      style={{ aspectRatio: '1/1' }}
+                      className='d-flex justify-content-center align-items-center border-start border-end border-bottom border-1 border-black'
+                    >
+                      {game.row1.cel2}
+                    </Col>
+                    <Col
+                      style={{ aspectRatio: '1/1' }}
+                      className='d-flex justify-content-center align-items-center border-start border-bottom border-1 border-black'
+                    >
+                      {game.row1.cel3}
+                    </Col>
+                  </Row>
+                  <Row xs={3}>
+                    <Col
+                      style={{ aspectRatio: '1/1' }}
+                      className='d-flex justify-content-center align-items-center border-top border-bottom border-end border-1 border-black'
+                    >
+                      {game.row2.cel1}
+                    </Col>
+                    <Col
+                      style={{ aspectRatio: '1/1' }}
+                      className='d-flex justify-content-center align-items-center border border-1 border-black'
+                    >
+                      {game.row2.cel2}
+                    </Col>
+                    <Col
+                      style={{ aspectRatio: '1/1' }}
+                      className='d-flex justify-content-center align-items-center border-start border-top border-bottom border-1 border-black'
+                    >
+                      {game.row2.cel3}
+                    </Col>
+                  </Row>
+                  <Row xs={3}>
+                    <Col
+                      style={{ aspectRatio: '1/1' }}
+                      className='d-flex justify-content-center align-items-center border-top border-end border-1 border-black'
+                    >
+                      {game.row3.cel1}
+                    </Col>
+                    <Col
+                      style={{ aspectRatio: '1/1' }}
+                      className='d-flex justify-content-center align-items-center border-start border-top border-end border-1 border-black'
+                    >
+                      {game.row3.cel2}
+                    </Col>
+                    <Col
+                      style={{ aspectRatio: '1/1' }}
+                      className='d-flex justify-content-center align-items-center border-start border-top border-1 border-black'
+                    >
+                      {game.row3.cel3}
+                    </Col>
+                  </Row>
+                </div>
               </div>
             );
           })}
